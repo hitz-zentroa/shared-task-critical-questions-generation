9c95658db7162a1ac9971adde4e865ed5b20de42
Blanca <blancacalvofigueras@gmail.com>
2025-02-06 11:32:12 +0100

    updated requirements

--- shared_task/eval_scripts/evaluation.py
+++ shared_task/eval_scripts/evaluation.py
@@ -1,11 +1,11 @@
 import json
-#from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer
 import numpy as np
 from collections import Counter
 import sys
 import argparse
 import logging
-from evaluate import load # if you don't want to use bleurt and you don't have it installed, you can comment this
+#from evaluate import load # if you don't want to use bleurt and you don't have it installed, you can comment this, gpu might be needed
 
 logger = logging.getLogger(__name__)
 
